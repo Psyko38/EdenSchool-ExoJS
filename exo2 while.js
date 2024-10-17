d20abe4822c487b7f19050985ad5d8f60b53721e
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2024-10-17 14:33:51 +0200

    (feat) : new code

---
+++ exo2 while.js
@@ -0,0 +1,5 @@
+let n = 91;
+while (n > 65) {
+	n = n - 1;
+	console.log(String.fromCharCode(n));
+}
---
+++ exo3 while.js
@@ -0,0 +1,6 @@
+let n = Number(prompt("votre nombre"));
+let output = 2 ** n;
+
+if (output >= 2 ** 8 && output <= 2 ** 10) {
+	console.log(output);
+}
